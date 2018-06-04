56333736340ae7d6725f13f38b5a518ba36098c5
tony <tony850421@gmx.es>
2018-06-04 10:11:41 +0800

    real time notification messages

--- src/js/controllers/inbox.js
+++ src/js/controllers/inbox.js
@@ -88,7 +88,59 @@ function InboxCtrl($scope, $rootScope, $state, $window, $timeout, localStorageSe
         }).catch(function (error) {
             alert(JSON.stringify(error));
         });
+        
+        var querySocket = new AV.Query('Message');
+        querySocket.include('sender');
+        querySocket.include('receiver');
+        querySocket.equalTo('receiver', user);
+        querySocket.subscribe().then(function (liveQuery) {
+            liveQuery.on('create', function (message) {
+                // add newDoingItem to doingList
+                console.log('Live query');
+                console.log(message);
 
+                var fullName = message.get('sender').get('fullName');
+                var releaseTime = (message.createdAt.getMonth() + 1) + '/' + message.createdAt.getDate() + '/' + message.createdAt.getFullYear();
+                var avatar = message.get('sender').get('avatarUrl');
+                var content = message.get('content');
+                var aux = content;
+                var unreaded = message.get('readedAdmin');
+                var unreadedCount= 0;
+                if (!unreaded){
+                    unreadedCount = 1;
+                }
+
+                if (content.length > 15){
+                    var aux = '';
+                    for (var t=0; t<15; t++){
+                        aux += content[t];
+                    }
+                    aux+= '...';
+                }
+                content = aux;
+                var id = message.get('sender').id;
+
+                console.log(message.get('sender').id);
+
+                var flagMessage = false;
+                $scope.inbox.forEach(function (msg) {
+                    if (msg.senderId == message.get('sender').id) {
+                        console.log('true tre');
+                        flagMessage = true;
+                        msg.content = content;
+                        if (!unreaded) {
+                            msg.unreadedCount += 1;                                
+                        }
+                    }
+                })
+
+                if (!flagMessage) {
+                    $scope.inbox.push({ fullName: fullName, releaseTime: releaseTime, avatar: avatar, content: content, senderId: id, unreadedCount: unreadedCount });
+                }
+
+                $scope.$apply();
+            });
+        });
     };
 
     $scope.init();
@@ -133,8 +185,6 @@ function InboxCtrl($scope, $rootScope, $state, $window, $timeout, localStorageSe
         queryOr.find().then(function (messages) {
 
             $scope.Messages = [];
-
-            
 
             messages.forEach(function (msg) {
 
