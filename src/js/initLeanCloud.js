2d1cb68884c4cf008d3c56caa6bdfde692444ac6
Alejandro Ravelo Julian <alejulian9119@gmail.com>
2019-01-22 15:33:03 +0800

    change database

--- src/js/initLeanCloud.js
+++ src/js/initLeanCloud.js
@@ -1,8 +1,8 @@
 // LeanCloud - 初始化 - 将这里的 APP_ID 和 APP_KEY 替换成自己的应用数据
 // https://leancloud.cn/docs/sdk_setup-js.html#初始化
 
-var APP_ID = 'qPv1WNQew34FTEuaGxEKHzqt-gzGzoHsz';
-var APP_KEY = 'W7HA63OOB6oWtMQPOj6Cenr7';
+var APP_ID = 'JFuqWjSPODy08snVJFtXD6IP-gzGzoHsz';
+var APP_KEY = 'vaUr4WV0p1nRFKlypzN374jx';
 
 // var APP_ID = 'qNFjWh6zS7D36L7lwPl7JAnN-gzGzoHsz';
 // var APP_KEY = 'Mizywm8n5MtGwJWaWpRAElF3';
